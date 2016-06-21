1f985dcfcc6f287cfad7a9531df9567ae23932ae
carluxhenrique@gmail.com <carlos*2014>
2016-06-20 22:04:45 -0300

    Criado as classes do modelo.

--- appprojetos/models.py
+++ appprojetos/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Participantes(models.Model):
+    nome = models.Charfield("Nome",max_length=255)
+    descricao = models.Charfield("Descrição",max_length=100)
+
+class AcompanhamentoAtividades(models.Model):
+    descricao = models.Charfield("Descrição", max_length=100)
+    data = models.DateField("Data", auto_now_add=True)
+    atividades = models.ForeignKey(Atividades,on_delete=models.PROTECT,verbose_name="Atividades")
+
+class Atividades(models.Model):
+    descricao = models.Charfield("Descrição", max_length=100)
+    dataInicio = models.DateField("Data Inicio",auto_now_add = True)
+    dataTermino = models.DateField("Data Termino", auto_now_add=True)
+    custos = models.DecimalField(max_digits=4,decimal_places=2,verbose_name="Custos")
+    projetos = models.ForeignKey(Projetos, on_delete=models.PROTECT,verbose_name="Projetos")
+
+class Projetos(models.Model):
+    titulo = models.Charfield("Título",max_length=100)
+    dataInicio = models.DateField("Data Inicio", auto_now_add=True)
+    dataTermino = models.DateField("Data Término", auto_now_add=True)
+    justificativa = models.Charfield("Justificativa", max_length=500)
+    metodologia = models.Charfield("Metodologia", max_length=800)
+    resultados = models.Charfield("Resultados Esperados", max_length=500)
+    participantes = models.ManyToManyField(Participantes,through="ParticipantesProjetos")
+
+class ParticipantesProjetos(models.Model):
+    participantes = models.ForeignKey(Participantes,on_delete=models.PROTECT)
+    projetos = models.ForeignKey(Projetos,on_delete=models.PROTECT)
