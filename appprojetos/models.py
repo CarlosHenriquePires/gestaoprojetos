2fb1a50d95ca54098c7c1f3763648481c3411ba6
carluxhenrique@gmail.com <carlos*2014>
2016-06-21 00:06:06 -0300

    Ordenando corretamente as classes --'

--- appprojetos/models.py
+++ appprojetos/models.py
@@ -6,18 +6,6 @@ class Participantes(models.Model):
     nome = models.CharField("Nome",max_length=255)
     descricao = models.CharField("Descrição",max_length=100)
 
-class AcompanhamentoAtividades(models.Model):
-    descricao = models.CharField("Descrição", max_length=100)
-    data = models.DateField("Data", auto_now_add=True)
-    atividades = models.ForeignKey(Atividades,on_delete=models.PROTECT,verbose_name="Atividades")
-
-class Atividades(models.Model):
-    descricao = models.CharField("Descrição", max_length=100)
-    dataInicio = models.DateField("Data Inicio",auto_now_add=True)
-    dataTermino = models.DateField("Data Término")
-    custos = models.DecimalField(max_digits=4,decimal_places=2,verbose_name="Custos")
-    projetos = models.ForeignKey(Projetos, on_delete=models.PROTECT,verbose_name="Projetos")
-
 class Projetos(models.Model):
     titulo = models.CharField("Título",max_length=100)
     dataInicio = models.DateField("Data Inicio", auto_now_add=True)
@@ -27,4 +15,15 @@ class Projetos(models.Model):
     resultados = models.CharField("Resultados Esperados", max_length=500)
     participantes = models.ManyToManyField(Participantes)
 
+class Atividades(models.Model):
+    descricao = models.CharField("Descrição", max_length=100)
+    dataInicio = models.DateField("Data Inicio",auto_now_add=True)
+    dataTermino = models.DateField("Data Término")
+    custos = models.DecimalField(max_digits=4,decimal_places=2,verbose_name="Custos")
+    projetos = models.ForeignKey(Projetos, on_delete=models.PROTECT,verbose_name="Projetos")
+
+class Acompanhamento(models.Model):
+    descricao = models.CharField("Descrição", max_length=100)
+    data = models.DateField("Data", auto_now_add=True)
+    atividades = models.ForeignKey(Atividades,on_delete=models.PROTECT,verbose_name="Atividades")
 
