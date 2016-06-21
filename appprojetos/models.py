27081d355e7f8eefed127a7ddad188618025e89f
MatheusFerrazz <matheusb994@gmail.com>
2016-06-21 20:33:57 -0300

    Corrigindo a inserção do projeto.

--- appprojetos/models.py
+++ appprojetos/models.py
@@ -7,6 +7,7 @@ class Participantes(models.Model):
     matricula = models.CharField("Matrícula",max_length=14,unique=True)
     descricao = models.CharField("Descrição",max_length=100)
 
+
 class Projetos(models.Model):
     titulo = models.CharField("Título",max_length=100)
     dataInicio = models.DateField("Data Inicio", auto_now_add=True)
@@ -15,6 +16,12 @@ class Projetos(models.Model):
     metodologia = models.CharField("Metodologia", max_length=800)
     resultados = models.CharField("Resultados Esperados", max_length=500)
     participantes = models.ManyToManyField(Participantes)
+
+'''
+class ParticipantesProjetos(models.Model):
+    projetos = models.ForeignKey(Projetos,on_delete=models.PROTECT)
+    participantes = models.ForeignKey(Participantes, on_delete=models.PROTECT)
+'''
 
 class Atividades(models.Model):
     descricao = models.CharField("Descrição", max_length=100)
@@ -27,5 +34,7 @@ class Acompanhamento(models.Model):
     descricao = models.CharField("Descrição", max_length=100)
     data = models.DateField("Data", auto_now_add=True)
     atividades = models.ForeignKey(Atividades,on_delete=models.PROTECT,verbose_name="Atividades")
+
+
 
 
--- populabd.py
+++ populabd.py
@@ -21,17 +21,35 @@ participante4.save()
 participante5.save()
 participante6.save()
 #Criando os projetos
-projetos1=Projetos(titulo="Lógica de Programação com Scratch",dataInicio="2016-06-21",
+projetos1=Projetos(titulo="Lógica de Programação com Scratch", dataInicio="2016-06-21",
                    dataTermino="2016-07-21",justificativa="Motivar os jovens a iniciar o estudo de programação",
                    metodologia="XXX",resultados="Criar artigos")
 projetos1.save()
+projetos2 = Projetos(titulo="Jiu-Jitsu: Lutando pela cidadenia", dataInicio="2016-08-12",
+                     dataTermino="2016-10-23", justificativa="Incentivar a prática desportiva",
+                     metodologia="XXX", resultados="Concientizar a sociedade e beneficiados do projeto")
+projetos2.save()
+projetos3 = Projetos(titulo="Robótica: do básico ao avançado", dataInicio="2016-08-05",
+                     dataTermino="2016-12-14", justificativa="Ajudar crianças a desenvolver de maneira lúdica a criatividade",
+                     metodologia="XXX", resultados="Reciclagem")
+projetos3.save()
 # Inserindo os participantes do projeto
 projetos1.participantes.add(participante1,participante2,participante3)
+projetos2.participantes.add(participante4,participante5,participante3)
+projetos3.participantes.add(participante1,participante2,participante6)
 
 # Criando Atividades
 atividades1=Atividades(descricao="Visita à Escola",dataInicio="2016-06-21",dataTermino="2016-06-22",custos=1,projetos=projetos1)
 atividades1.save()
+atividades2=Atividades(descricao="Prepatrando o Tatame",dataInicio="2016-08-21",dataTermino="2016-08-22",custos=1,projetos=projetos2)
+atividades2.save()
+atividades3=Atividades(descricao="Separando o Material",dataInicio="2016-09-21",dataTermino="2016-09-22",custos=1,projetos=projetos3)
+atividades3.save()
 
 # Criando o acompanhamento das atividades
 acompanhamento1=Acompanhamento(descricao="Manutenção dos Laboratórios",data="2016-06-22",atividades=atividades1)
 acompanhamento1.save()
+acompanhamento2=Acompanhamento(descricao="Limpando Sala de treinos",data="2016-08-23",atividades=atividades2)
+acompanhamento2.save()
+acompanhamento3=Acompanhamento(descricao="Separar material para aulas práticas",data="2016-09-24",atividades=atividades3)
+acompanhamento3.save()
