64b5bc14cb76e3c7c97244530ee6ae1b3d4f44a0
carluxhenrique@gmail.com <carlos*2014>
2016-06-21 21:32:39 -0300

    criando consultas.py

---
+++ appprojetos/consultas.py
@@ -0,0 +1,10 @@
+from appprojetos import *
+
+# Listar todos os projetos com seus respectivos membros (Valor 0,2).
+print()
+
+# Listar todas as atividades que tenham sido executadas no mês de maio de 2015.
+
+# Listar todos as pessoas que fazem parte do Staff da Universidade cujo o nome começa com a letra A.
+
+# Listar o custo total de cada projeto.
