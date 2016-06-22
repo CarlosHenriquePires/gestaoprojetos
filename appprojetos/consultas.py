ad939141e7c4ea67ad56c036774c8759662c2973
MatheusFerrazz <matheusb994@gmail.com>
2016-06-21 23:42:43 -0300

    Quase pronto!

--- appprojetos/consultas.py
+++ appprojetos/consultas.py
@@ -2,14 +2,22 @@ from appprojetos.models import *
 
 # Listar todos os projetos com seus respectivos membros.
 
-projetos = Projetos.objects.all()
-for projeto in projetos:
-    print('titulo: ',projeto.titulo, )
+for projeto in Projetos.objects.all():
+    lista = []
+    for participante in projeto.participantes.iterator():
+        lista.append(participante.nome)
+    print('Projeto: ',projeto.titulo,  '\nParticipantes: ', lista)
 
-#print()
 
 # Listar todas as atividades que tenham sido executadas no mês de maio de 2015.
 
+for atividade in Atividades.objects.filter(dataInicio__year=2015,dataInicio__month=5).distinct():
+    print(atividade.descricao)
+
+
 # Listar todos as pessoas que fazem parte do Staff da Universidade cujo o nome começa com a letra A.
+
+for participantes in Participantes.objects.filter(nome__startswith='A'):
+    print(participantes.nome)
 
 # Listar o custo total de cada projeto.
